96695f5f802da7c446a2cb699edc46ed67d8f7e0
VozzRoman <looper0500@ukr.net>
2023-02-08 19:31:12 +0200

    adding

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { ContactList } from './ContcatList/ContactList';
 import { useState, useEffect } from 'react';
 import { Filter } from './Filter/Filter';
@@ -7,11 +6,15 @@ import { Background } from './Container/Background';
 import { Box } from './Container/Box';
 
 export const App = () => {
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) ?? ''
+  );
   const [filter, setFilter] = useState('');
   console.log(contacts);
 
-  useEffect(() => {});
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
   const hendlerFormData = (name, number, numberId) => {
     console.log(name, number, numberId); // data
@@ -72,110 +75,3 @@ export const App = () => {
     </Background>
   );
 };
-
-// import { ContactList } from './ContcatList/ContactList';
-
-// import { Filter } from './Filter/Filter';
-
-// export class App extends Component {
-//   static LOCAL__KEY = 'contacts';
-
-//     state = {
-//       contacts: [],
-
-//       filter: '',
-//     };
-
-//   //-----------------------------LocalStorage------------------------------//
-
-//     componentDidMount() {
-//       console.log('didAmount');
-//       const contact = localStorage.getItem(this.LOCAL__KEY);
-//       const parsedContacts = JSON.parse(contact);
-//       if (parsedContacts) {
-//         this.setState({
-//           contacts: parsedContacts,
-//         });
-//       }
-//       console.log(parsedContacts);
-//     }
-
-//     componentDidUpdate(prevProps, prevState) {
-//       console.log('didUpdate');
-//       if (this.state.contacts !== prevState.contacts) {
-//         console.log('добавил контакт');
-//         localStorage.setItem(
-//           this.LOCAL__KEY,
-//           JSON.stringify(this.state.contacts)
-//         );
-//       }
-//     }
-//   //----------------------------------------------------------------------//
-//    hendlerFormData = (name, number, numberId) => {
-//     console.log({ name, number }); // data
-//     const addContact = {
-//       id: numberId,
-//       name,
-//       number,
-//     };
-//     this.setState(prevState => ({
-//       contacts: [addContact, ...prevState.contacts],
-//     }));
-//   };
-
-//   deleteContact = contactId => {
-//     this.setState(prevState => ({
-//       contacts: prevState.contacts.filter(item => item.id !== contactId),
-//     }));
-//   };
-
-//   onchangeFilter = e => {
-//     this.setState({ filter: e.currentTarget.value });
-//   };
-
-//   getFilteredContacts = () => {
-//     const normaLized = this.state.filter.toLocaleLowerCase();
-//     const sameName = this.state.contacts.filter(item =>
-//       item.name.toLocaleLowerCase().includes(normaLized)
-//     );
-//     return sameName;
-//   };
-
-//   render() {
-//      const filteredContacts = this.getFilteredContacts();
-
-//     return (
-//       <Background>
-//         <Box
-//           m="0 auto"
-//           maxWidth="500px"
-//           background="orange"
-//           borderRadius="12px"
-//           p="20px"
-//           boxShadow="3px 4px 6px #888888"
-//         >
-//           <h1>Phonebook</h1>
-//           <ContactForm
-//             onSubmit={hendlerFormData}
-//             contacts={this.state.contacts}
-//           />
-//           {this.state.contacts.length === 0 ? (
-//             <h2>The phonebook is empty</h2>
-//           ) : (
-//             <>
-//               <h2>Contacts</h2>
-//               <Filter
-//                 onChange={this.onchangeFilter}
-//                 value={this.state.filter}
-//               />
-//               <ContactList
-//                 phoneContact={filteredContacts}
-//                 onDeleteContact={this.deleteContact}
-//               />
-//             </>
-//           )}
-//         </Box>
-//       </Background>
-//     );
-//   }
-// }
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -20,7 +20,7 @@ export const ContactForm = ({ contacts, onSubmit }) => {
         setNumber(e.currentTarget.value);
         break;
       default:
-        return null;
+        return console.log('не та строка');
     }
   };
 
@@ -81,88 +81,3 @@ export const ContactForm = ({ contacts, onSubmit }) => {
     </Forms>
   );
 };
-
-// export class ContactForm extends Component {
-//   static propTypes = {
-//     onSubmit: PropTypes.func.isRequired,
-//   };
-
-//   state = {
-//     name: '',
-//     number: '',
-//   };
-
-//   handleChange = e => {
-//     console.log(e.currentTarget);
-//     console.log(e.currentTarget.name);
-//     console.log(e.currentTarget.value);
-//     this.setState({
-//       [e.currentTarget.name]: e.currentTarget.value,
-//     });
-//   };
-
-//   hendleSubmit = e => {
-//     e.preventDefault();
-//     console.log(this.state);
-
-//     const { name, number } = this.state;
-//     const { contacts } = this.props;
-//     if (contacts.find(item => item.name === name)) {
-//       alert(`${name} is already in contacts.`);
-//       return;
-//     } else if (contacts.find(item => item.number === number)) {
-//       alert(`${number} is already in contacts.`);
-//       return;
-//     } else if (!/\d{3}[-]\d{2}[-]\d{2}/g.test(number)) {
-//       alert('Enter the correct number phone!');
-//       return;
-//     }
-
-//     this.props.onSubmit({ name, number }, shortid.generate());
-//     this.resetForm();
-//   };
-//   resetForm = () => {
-//     this.setState({
-//       id: shortid.generate(),
-//       name: '',
-//       number: '',
-//     });
-//   };
-
-//   render() {
-//     const { name, number } = this.state;
-//     return (
-//       <Forms onSubmit={this.hendleSubmit}>
-//         <FildName htmlFor="name">
-//           Name
-//           <EnterName
-//             value={name}
-//             onChange={this.handleChange}
-//             id="name"
-//             type="text"
-//             name="name"
-//             placeholder="Enter name"
-//             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-//             required
-//           />
-//         </FildName>
-//         <FildName htmlFor="number">
-//           Number
-//           <EnterName
-//             value={number}
-//             onChange={this.handleChange}
-//             id="number"
-//             type="tel"
-//             name="number"
-//             placeholder="Enter number"
-//             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-//             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-//             required
-//           />
-//         </FildName>
-//         <Button type="submit">Add contact</Button>
-//       </Forms>
-//     );
-//   }
-// }
